b766e38ddf1a95927c8920431949231baf6de4ee
yanjingyun <326015540@qq.com>
2020-11-09 15:29:25 +0800

    fix(swagger):接口使用@PostMapping替换@RequestMapping注解

--- sop-example/my-sop-demo/src/main/java/com/yjy/controller/MyTestController.java
+++ sop-example/my-sop-demo/src/main/java/com/yjy/controller/MyTestController.java
@@ -1,5 +1,6 @@
 package com.yjy.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.gitee.sop.servercommon.annotation.BizCode;
 import com.gitee.sop.servercommon.annotation.Open;
 import com.yjy.vo.MyParam;
@@ -8,8 +9,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * 签名验证通过后，到达这里进行具体的业务处理。
@@ -25,7 +27,7 @@ public class MyTestController {
 
     // http://localhost:2222/mydemo/get
     // 原生的接口，可正常调用
-    @RequestMapping("/get")
+    @GetMapping("/get")
     public MyVo get() {
         MyVo result = new MyVo();
         result.setId(1L);
@@ -41,12 +43,13 @@ public class MyTestController {
             @BizCode(code = "100001", msg = "姓名错误", solution = "填写正确的姓名"),
             @BizCode(code = "100002", msg = "备注错误", solution = "填写正确备注"),
     })
-    @RequestMapping("/get/v1")
-    public MyVo get_v1(MyParam param) {
+    @PostMapping("/get/v1")
+    public MyVo get_v1(@Valid @RequestBody MyParam param) {
         MyVo story = new MyVo();
-        story.setId(1L);
-        story.setName("海底小纵队(mydemo.get1.0), " + "param:" + param + ", port:" + port);
-        story.setAge(22);
+        story.setId(2L);
+        System.out.println(JSON.toJSONString(param));
+        story.setName("海底小纵队(mydemo.get1.0), " + "param:" + JSON.toJSONString(param) + ", port:" + port);
+        story.setAge(32);
         return story;
     }
 }
